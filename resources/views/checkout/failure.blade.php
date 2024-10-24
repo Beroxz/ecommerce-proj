60fc8b538b8192fea37fe72e402a0e18d7bcc1c8
ratchapol.kitt <ratchapol.ktt@gmail.com>
2024-10-25 01:45:16 +0700

    fix status after check out

--- resources/views/checkout/failure.blade.php
+++ resources/views/checkout/failure.blade.php
@@ -1,6 +1,12 @@
 <x-app-layout>
     <div class="w-[400px] mx-auto bg-red-500 py-2 px-3 text-white rounded">
         <h1>Your payment was not successful!!</h1>
-        <p>{{$message ?? ''}}</p>
+        <p>{{ $message ?? '' }}</p>
     </div>
+
+    <script>
+        setTimeout(function() {
+            window.location.href = "{{ route('order.index') }}";
+        }, 1500);
+    </script>
 </x-app-layout>
--- resources/views/checkout/success.blade.php
+++ resources/views/checkout/success.blade.php
@@ -1,5 +1,11 @@
 <x-app-layout>
     <div class="w-[400px] mx-auto bg-emerald-500 py-2 px-3 text-white rounded">
-        {{$customer->name}}, Your order has been completed!!
+        {{ $customer->name }}, Your order has been completed!!
     </div>
+
+    <script>
+        setTimeout(function() {
+            window.location.href = "{{ route('order.index') }}";
+        }, 1500);
+    </script>
 </x-app-layout>
